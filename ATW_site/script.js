c5331fefcafcf69605ba54bfe84eec71f7f61361
leireii <irizarryleire@gmail.com>
2019-09-01 07:39:46 -0400

    Add files via upload

---
+++ ATW_site/script.js
@@ -0,0 +1,16 @@
+
+
+
+// $(document).ready(function () {
+
+//   $('#collapseButton').on('click', function () {
+//       $('#navbar').toggleClass('active');
+//       $('#collapseButton').toggleClass('active');
+//       $('#leftarrow').toggleClass('active');
+//   });
+
+// });
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
